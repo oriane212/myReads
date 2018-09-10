219be2cc1c2fec251c12d07c3beba61e4dea0325
Oriane <>
2018-09-10 15:21:09 -0400

    add BooksAPI.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from '../../../Library/Caches/typescript/2.9/node_modules/@types/react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!</h1>
+          <h1 className="App-title">Welcome to React!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
